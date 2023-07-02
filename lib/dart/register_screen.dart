871e146c53fe2110ed69bbf492756b33b94cf141
Omar <oh63723@gmail.com>
2023-07-02 16:39:44 +0300

    Add massages_screen.dart

--- lib/dart/register_screen.dart
+++ lib/dart/register_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'massages_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const MassagesScreen(),
     );
   }
 }
